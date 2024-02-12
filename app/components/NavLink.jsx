1d9946ba1fcff612a1d986a47f308113054db5cc
joe <jreeve36@gmail.com>
2024-02-12 13:12:42 -1000

    Change how nav scroll works

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import Link from "next/link";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 function NavLink({ href, title }) {
   return (
-    <Link
+    <ScrollLink
+      to={href}
+      smooth={true}
+      duration={500}
       href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
     >
       {title}
-    </Link>
+    </ScrollLink>
   );
 }
 
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -8,15 +8,15 @@ import MenuOverlay from "./MenuOverlay";
 const navLinks = [
   {
     title: "About",
-    path: "#about",
+    path: "about",
   },
   {
     title: "Projects",
-    path: "#projects",
+    path: "projects",
   },
   {
     title: "Contact",
-    path: "#contact",
+    path: "contact",
   },
 ];
 
@@ -66,17 +66,11 @@ function Navbar() {
             </button>
           )}
         </div>
-        <div
-          className="menu hidden md:block md:w-auto"
-          id="navbar"
-        >
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
             {navLinks.map((link, index) => (
               <li key={index}>
-                <NavLink
-                  href={link.path}
-                  title={link.title}
-                />
+                <NavLink href={link.path} title={link.title} />
               </li>
             ))}
           </ul>
